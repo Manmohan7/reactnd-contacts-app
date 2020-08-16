fe3e0e4c3ac5d6a9fb3da615bce3d8fdf62fb1a0
Manmohan7 <er.manmohansingh7@gmail.com>
2020-08-16 14:50:55 +0530

    propTypes update and indentation

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,38 +5,38 @@ class ListContacts extends Component {
   render() {
     return (
       <ol className="contact-list">
-      {this.props.contacts.map((contact) => (
-        <li key={contact.id} className="contact-list-item">
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
 
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}
-          ></div>
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            ></div>
 
-          <div className="contact-details">
-            <p> {contact.name} </p>
-            <p> {contact.handle} </p>
-          </div>
+            <div className="contact-details">
+              <p> {contact.name} </p>
+              <p> {contact.handle} </p>
+            </div>
 
-          <button
-            className="contact-remove"
-            onClick={() => this.props.onDeleteContact(contact)}
-          >
-            Remove
+            <button
+              className="contact-remove"
+              onClick={() => this.props.onDeleteContact(contact)}
+            >
+              Remove
             </button>
 
-        </li>
-      ))}
-    </ol>
+          </li>
+        ))}
+      </ol>
     )
   }
 }
 
-ListContacts.PropTypes({
+ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
   onDeleteContact: PropTypes.func.isRequired,
-})
+}
 
 export default ListContacts
